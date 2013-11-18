85960cbb323aa3b683915c116fb81eded437c67f
free <747549945@qq.com>
2013-11-18 09:00:41 +0800

    初始化

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.1'
+
+gem 'sass-rails', '~> 4.0.0'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+group :develpoment do
+  gem 'sqlite3'
+end
+
+# group :production do
+
+# end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ibiancheng::Application.config.secret_key_base = '17f2a77da52b872b55ea40a82aa32dae4308c7965d56167d481b70ea37e9c01c1005265d660f7b6250841a46707acdabae33b99c18a22ce6c1de8717d8e97900'
